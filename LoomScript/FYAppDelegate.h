0424d49ffccbdf73ddd618927f323f690fd473d6
Raul Menezes <raul.fuji@gmail.com>
2013-03-30 18:43:32 -0300

    Initial Commit

---
+++ LoomScript/FYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FYAppDelegate.h
+//  LoomScript
+//
+//  Created by Raul Menezes on 30/03/13.
+//  Copyright (c) 2013 Foo Yeah. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FYAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ LoomScript/FYAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  FYAppDelegate.m
+//  LoomScript
+//
+//  Created by Raul Menezes on 30/03/13.
+//  Copyright (c) 2013 Foo Yeah. All rights reserved.
+//
+
+#import "FYAppDelegate.h"
+
+@implementation FYAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
